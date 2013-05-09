944a352a4517a09014442db34ca19b4dca711c6d
Sean A. Hanley <yadyn@yahoo.com>
2013-05-09 17:07:49 -0600

    Fix custom repo
    Add new repo interface methods.

--- JabbR/Services/CustomRepository.cs
+++ JabbR/Services/CustomRepository.cs
@@ -159,6 +159,11 @@ namespace JabbR.Services
                 .AsQueryable();
         }
 
+        public IQueryable<Notification> GetNotificationsByUser(ChatUser user)
+        {
+            return _notifications.Where(n => n.UserKey == user.Key).AsQueryable();
+        }
+
         public IQueryable<ChatMessage> GetMessagesByRoom(ChatRoom room)
         {
             return room.Messages.AsQueryable();
@@ -199,6 +204,11 @@ namespace JabbR.Services
                 return identity.User;
             }
             return null;
+        }
+
+        public Notification GetNotificationById(int notificationId)
+        {
+            return _notifications.SingleOrDefault(n => n.Key == notificationId);
         }
 
         public ChatClient GetClientById(string clientId, bool includeUser = false)
