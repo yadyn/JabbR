bb6964046a47033c66cc5d53438f2d6e845d8086
cbellinger <coeybellinger@gmail.com>
2017-06-08 16:05:50 -0500

    Spoiler improvement (#10)
    * Improve spoiler command to allow infinite nesting and increased mixing of hiding/non-hiding.

--- JabbR/Commands/SpoilerCommand.cs
+++ JabbR/Commands/SpoilerCommand.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using JabbR.Models;
 using Microsoft.AspNet.SignalR;
+using JabbR.Extensions;
 
 namespace JabbR.Commands
 {
@@ -12,14 +13,11 @@ namespace JabbR.Commands
     {
         private const string _hiddenFormat = @"<s>{0}</s>";
 
-        private readonly Regex _hiddenPattern = new Regex(@"\[(.+)\]");
-
         public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
         {
             ChatRoom room = context.Repository.VerifyUserRoom(context.Cache, callingUser, callerContext.RoomName);
 
             room.EnsureOpen();
-
             if (args.Length == 0)
             {
                 throw new HubException(LanguageResources.Spoiler_ActionRequired);
@@ -28,30 +26,16 @@ namespace JabbR.Commands
             var callingRoom = context.Repository.GetRoomByName(callerContext.RoomName);
             var message = System.Net.WebUtility.HtmlEncode(String.Join(" ", args));
 
-            var matches = _hiddenPattern.Matches(message).Cast<Match>().ToList();
 
-            if (matches.Any())
+
+            if (message.Contains('[') && message.Contains(']') && message.HasValidBrackets())
             {
-                foreach (var match in matches)
-                {
-                    // Case 1: mixed visible/hidden
-                    message = message.Replace(match.Value, String.Format(_hiddenFormat, match.Groups[1].Value));
-
-                    var subMatches = _hiddenPattern.Matches(match.Groups[1].Value).Cast<Match>().ToList();
-
-                    // Case 2: mixed visible/hidden/morehidden
-                    if (subMatches.Any())
-                    {
-                        foreach (var subMatch in subMatches)
-                        {
-                            message = message.Replace(subMatch.Value, String.Format(_hiddenFormat, subMatch.Groups[1].Value));
-                        }
-                    }
-                }
+                //Case 1: spoilers wrapped with brackets.
+                message.Replace("[", "<s>").Replace("]", "</s>");
             }
             else
             {
-                // Case 3: non-mixed, all hidden
+                // Case 2: non-mixed, all hidden
                 message = String.Format(_hiddenFormat, message);
             }
 
---
+++ JabbR/Extensions/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JabbR.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool HasValidBrackets(this string input)
+        {
+            var bracketsBalanced = true;
+
+            var brackets = new Stack<char>();
+            var bracketChars = new Dictionary<char, char>() { { '[', ']' } };
+
+            foreach (var chr in input)
+            {
+                if (bracketChars.ContainsKey(chr))
+                {
+                    brackets.Push(chr);
+                }
+                else if (bracketChars.ContainsValue(chr))
+                {
+                    var set = new KeyValuePair<char, char>(brackets.Pop(), chr );
+                    if (!bracketChars.Contains(set))
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (brackets.Count > 0)
+            {
+                bracketsBalanced = false;
+            }
+            return bracketsBalanced;
+        }
+    }
+}
